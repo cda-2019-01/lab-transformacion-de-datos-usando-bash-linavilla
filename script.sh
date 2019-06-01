b493a8113328ca4488f603829fcbdf52a32b0fb3
Lina Maria Villa <50224761+linavilla@users.noreply.github.com>
2019-06-01 13:34:43 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,22 @@
-# Escriba su código aquí
+#Lab2
+for file in $(find . -name '*.csv');
+do
+    fn="$(basename -- ${file})";
+    i=0
+    while read line || [ -n "$line" ];
+    do
+        if [ ${#line} -gt 1 ]
+        then
+            ((i++))
+            cadenas=$(echo $line | tr "," "\n");
+            upp=$(echo $line | head -c 1);
+            for cad in $cadenas
+            do
+                if [[ "$cad" =~ ...:. ]]
+                then
+                    echo "$fn,$i,$upp,"$cad"";
+                fi
+            done
+        fi
+    done < $fn
+done
